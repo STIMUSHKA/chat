0389536d795d070693b0cf1c41af2d4997b350f3
STIMUSHKA <stimushka@list.ru>
2024-05-13 04:30:58 +0700

    login page design

--- angular/chat/src/app/login/login.component.ts
+++ angular/chat/src/app/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Используйте styleUrls для стилей
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Login form is valid');
+      console.log('Email:', this.loginForm.value.email);
+      console.log('Password:', this.loginForm.value.password);
+      // Здесь вы можете добавить логику для обработки входа пользователя
+    } else {
+      console.log('Login form is invalid');
+      // Здесь можно добавить обработку невалидной формы
+    }
+  }
 }
--- angular/chat/src/app/navbar/navbar.component.ts
+++ angular/chat/src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor(private router: Router) { }
+
+  public redirectLogin() {
+    this.router.navigate(['/login'])
+  }
 }
